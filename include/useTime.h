c6f8200760b45109babdd8639a54730c29c7bfdd
magicep0t <holladiewaldfee2@gmail.com>
2021-09-16 00:41:57 +0200

    adding basic NTP functionality

--- include/useTime.h
+++ include/useTime.h
@@ -1,3 +1,5 @@
+// This file is used to set correct time stamps for the transmitted data to the server
+
 #ifndef USETIME_H
 #define USETIME_H
 
@@ -10,10 +12,11 @@
 class UseTime{
     public:
         UseTime();
-        
+    private:
         const char* ntpServer;
         const long utcOffsetInSeconds;
         WiFiUDP ntpUDP;
-        NTPClient timeClient;
+        // NTPClient timeClient;    no idea why this is not allowed to be declared here. ..
+        const char** daysOfTheWeek;
 };
 #endif
--- src/useTime.cpp
+++ src/useTime.cpp
@@ -1,10 +1,23 @@
 #include "../include/useTime.h"
+#include "../include/configStatic.h"
+#include <Arduino.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-UseTime::UseTime()
+UseTime::UseTime() : ntpServer("europe.pool.ntp.org"),
+                     utcOffsetInSeconds(7200)   // UTC offset in sekunden // Berlin = UTC+2 -> 3600*2 sekunden
+                    //  ntpUDP(WiFiUDP())
 {
-    const char *ntpServer = "europe.pool.ntp.org";
-    // UTC offset in sekunden // Berlin = UTC+2 -> 3600*2 sekunden
-    const long utcOffsetInSeconds = 7200;
+    const char daysOfTheWeek[7][12] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    // daysOfTheWeek = new const char*[7];
+    // daysOfTheWeek[0] = {"Monday"};
+    // daysOfTheWeek[1] = {"Tuesday"};
+    // daysOfTheWeek[2] = {"Wednesday"};
+    // daysOfTheWeek[3] = {"Thursday"};
+    // daysOfTheWeek[4] = {"Friday"};
+    // daysOfTheWeek[5] = {"Saturday"};
+    // daysOfTheWeek[6] = {"Sunday"};
     ntpUDP = WiFiUDP();
-    timeClient = NTPClient(ntpUDP, ntpServer, utcOffsetInSeconds);
+    NTPClient timeClient(ntpUDP, ntpServer, utcOffsetInSeconds);
 }
